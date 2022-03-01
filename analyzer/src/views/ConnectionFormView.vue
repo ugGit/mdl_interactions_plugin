66903b772af09a836b81f3df43b283c4afa29a6c
Uchendu Nwachukwu <uchendu.nwachukwu@edu.hefr.ch>
2022-03-01 16:03:59 +0100

    feat: connection form placeholder

--- analyzer/src/views/ConnectionFormView.vue
+++ analyzer/src/views/ConnectionFormView.vue
@@ -1,12 +1,52 @@
 <template>
-  <v-row>
-    hi
-  </v-row>
+  <v-form>
+    <h2>Connect to your Moodle instance</h2>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-text-field v-model="url" label="URL" required></v-text-field>
+          <v-text-field
+            v-model="username"
+            label="username"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="password"
+            type="password"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn block color="success" @click="establishConnection()"
+            >Login</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
-
 export default {
-  name: 'ConnectionView',
-}
+  name: "ConnectionView",
+  data: function () {
+    return {
+      valid: false,
+      url: "",
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    establishConnection: () => {
+      // minimal validation
+      if (!(this.url && this.username && this.password)) {
+        console.log("Missing content for form");
+      }
+    },
+  },
+};
 </script>
