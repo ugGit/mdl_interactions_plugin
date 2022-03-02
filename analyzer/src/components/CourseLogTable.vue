9a07309dc4765ee0a30c0cb0ce5c1828d91befd9
Uchendu Nwachukwu <uchendu.nwachukwu@edu.hefr.ch>
2022-03-02 12:05:27 +0100

    feat: add vuetify3 table (not optimal)

---
+++ analyzer/src/components/CourseLogTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card>
+    <v-card-header> Events </v-card-header>
+    <v-table v-if="courseLog.length > 0">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th
+              v-for="columnHeader in Object.keys(courseLog[0])"
+              :key="'header-' + columnHeader"
+            >
+              {{ columnHeader }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, index) in courseLog" :key="'row-' + index">
+            <td v-for="(col, i) in row" :key="'row-' + index + 'col-' + i">
+              {{ col }}
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-table>
+    <div v-else>
+      No data imported or matching the currently selected filters.
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "CourseLogTable",
+  props: ["courseLog", "isLoading"],
+};
+</script>
